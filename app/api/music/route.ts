4c78c7b9e568917fc938fb9dbdac15365701e276
akins-dev <bobemmanuel503@gmail.com>
2023-10-30 12:48:04 +0100

    fix: still debugging

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -5,7 +5,7 @@ import { userId } from "@/lib/get-user-id";
 import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit"
 
 const replicate = new Replicate({
-    auth: process.env.REPLICATE_API_TOKEN!
+    auth: process.env.REPLICATE_API_TOKEN || ""
 })
 
 export async function POST(
@@ -15,6 +15,8 @@ export async function POST(
         const userID = await  userId();
         const body = await req.json();
         const { prompt } = body;
+
+        console.log(prompt)
 
         if (!userID) {
             return new NextResponse("Unauthorized", { status: 401 });
@@ -30,6 +32,8 @@ export async function POST(
             return new NextResponse("Free trial has expired.", { status: 403 })
         }
 
+        console.log(freeTrial)
+
         const response = await replicate.run(
             "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
             {
@@ -38,6 +42,8 @@ export async function POST(
                 }
             }
         );
+
+        console.log(response)
 
         await incrementApiLimit();
 
